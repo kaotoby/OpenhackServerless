8b345a592195a51b1bb908f1455f7217a619f4d3
Toby Kao <toby850817@gmaill.com>
2018-10-23 16:00:48 -0500

    1. Add GetRating

---
+++ Kaotoby.Openhack.Serverless.Chanllage2/GetRating.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Kaotoby.Openhack.Serverless.Chanllage2.Models;
+
+namespace Kaotoby.Openhack.Serverless.Chanllage2
+{
+    public static class GetRating
+    {
+        [FunctionName("GetRating")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "c2",
+                collectionName: "Ratings",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "{Query.ratingId}")]Rating rating,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (rating == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                return new JsonResult(rating);
+            }
+        }
+    }
+}
---
+++ Kaotoby.Openhack.Serverless.Chanllage2/GetRatings.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Kaotoby.Openhack.Serverless.Chanllage2.Models;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+
+namespace Kaotoby.Openhack.Serverless.Chanllage2
+{
+    public static class GetRatings
+    {
+        [FunctionName("GetRatings")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "c2",
+                collectionName: "Ratings",
+                ConnectionStringSetting = "CosmosDBConnection")]
+                DocumentClient documentClient,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            Guid userId;
+            if (!Guid.TryParse(req.Query["userId"], out userId))
+            {
+                return new BadRequestResult();
+            }
+
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("c2", "Ratings");
+            List<Rating> ratings = documentClient.CreateDocumentQuery<Rating>(collectionUri)
+                .Where(rating => rating.userId == userId)
+                .ToList();
+
+            return new JsonResult(ratings);
+        }
+    }
+}
